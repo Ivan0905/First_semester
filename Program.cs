cba440d017a25270f6dbff0ec5211479fa707a7e
Фарафонов Иван Алексеевич <i.farafonov@ktpproekt.local>
2022-11-29 08:42:46 +0300

    добавлен метод сортировки первого массива и формирования нового массива согласно условию

--- Program.cs
+++ Program.cs
@@ -7,3 +7,28 @@ for (int i = 0; i < count; i++)
     Console.WriteLine($"Введите строку {i + 1}");
     array[i] = Console.ReadLine()!;
 }
+
+
+
+string[] ArraySort(string[] arr)
+{
+    int num = 0;
+    for (int j = 0; j < arr.Length; j++)
+    {
+        if (arr[j].Length <=3)
+        {
+            num++;
+        }
+    }
+    string[] array2 = new string[num];
+    num = 0;
+    for (int k = 0; k < arr.Length; k++)
+    {
+        if (arr[k].Length <= 3)
+        {
+            array2[num]=arr[k];
+            num++;
+        }
+    }
+    return array2;
+}
