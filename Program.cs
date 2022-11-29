cbe69a3a3b28a972378919a1fd01cc007f003143
Фарафонов Иван Алексеевич <i.farafonov@ktpproekt.local>
2022-11-29 08:40:38 +0300

    добавлен ввод строк с  терминала и заполнение первого массива

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите количество строк");
+int count = int.Parse(Console.ReadLine()!);
+string[] array = new string[count];
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"Введите строку {i + 1}");
+    array[i] = Console.ReadLine()!;
+}
